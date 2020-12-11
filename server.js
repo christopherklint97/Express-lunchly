8187742421ac838eebd22df91db000df2b7d7207
Christopher Klint <christopher.klint@gmail.com>
2020-12-11 11:50:20 +0100

    change port if on heroku

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 
 const app = require("./app");
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("listening on 3000");
 });
